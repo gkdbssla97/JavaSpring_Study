5ca5cfca652aea6d7dc68c39a63131c20ec993dd
Hayoon <gkdbssla97@gmail.com>
2022-04-30 20:58:04 +0900

    data-jpa

---
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,15 @@
+package study.datajpa;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Getter
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
