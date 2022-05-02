37e30133cd85671df95fc77aa3a715b7aac4d637
Hayoon <gkdbssla97@gmail.com>
2022-05-02 15:04:01 +0900

    Initial commit

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,37 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 저장
+            Member member = new Member();
+            member.setUsername("MemA");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username", "member1");
+            Member singleResult = query.getSingleResult();
+            System.out.println(singleResult);
+            
+            //Query query1 = em.createQuery("select m.age, m.username from Member m", Member.class);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/jpql/Order.java
@@ -0,0 +1,28 @@
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERS")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "ORDER_ID")
+//    private Order order
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+}
